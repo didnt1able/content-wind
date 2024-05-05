d3be9b80d4c1dfc5bf5bff76b8ef8e19b4444af9
didnt1able <didnt1able@pm.me>
2024-05-05 16:25:53 -0400

    Update swiper.vue

--- components/content/swiper.vue
+++ components/content/swiper.vue
@@ -13,10 +13,9 @@
     :modules="modules"
     class="mySwiper"
   >
-    <swiper-slide><img src="1"</swiper-slide>
-    <swiper-slide><img src="2"</swiper-slide>
-    <swiper-slide><img src="3"</swiper-slide>
-    <swiper-slide><img src="4"</swiper-slide>
+    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide>
+    <swiper-slide>Slide 3</swiper-slide>
    
   </swiper>
 </template>
